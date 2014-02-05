76f5acbbd70d3db692998c07a81cc1939d7e4048
hfaran <hamzafaran@hotmail.com>
2014-02-04 23:59:21 -0800

    Add create_room to client

--- client.py
+++ client.py
@@ -3,18 +3,23 @@ import requests
 import json
 import getpass
 
+DEBUG = True
+
 class AuthenticationError(Exception):
 
     """AuthenticationError"""
 
+
 class CutthroatAPI(object):
+
     """CutthroatAPI"""
+
     def __init__(self, base_url="http://localhost:8888", username=None):
         self.base_url = base_url
 
-        username = username if username else raw_input("Username: ")
+        self.username = username if username else raw_input("Username: ")
         password = getpass.getpass()
-        self.cookies = self._authenticate(username, password)
+        self.cookies = self._authenticate(self.username, password)
 
     def _authenticate(self, username, password):
         """Login with `username` and `password`"""
@@ -25,5 +30,23 @@ class CutthroatAPI(object):
                 "password": password
             })
         )
-        logging.info("{}\n{}".format(r, r.json()))
+        if DEBUG: print("{}\n{}".format(r, r.json()))
         return r.cookies
+
+    def create_room(self):
+        name = raw_input("Name of the room:")
+        password = raw_input("(Optional) Password to the room if you "
+                             "wish to keep entry restricted to players who "
+                             "know the password: ")
+        owner=self.username
+
+        data = dict(name=name, owner=owner)
+        if password:
+            data["password"] = password
+
+        r = requests.post(
+            self.base_url + "/api/room/createroom",
+            data=json.dumps(data)
+        )
+        logging.info("{}\n{}".format(r, r.json()))
+        return r.json()
