e8f3da5560a8dce40d0bed01b3bdd01552e17726
Steven Erenst <stevenerenst@gmail.com>
2014-02-09 02:31:24 -0800

    modifying templates

--- cutthroat/views/room.py
+++ cutthroat/views/room.py
@@ -1,7 +1,6 @@
 from tornado import template
 
 from cutthroat.handlers import ViewHandler
-from cutthroat import templates as ct_templates
 
 
 class Join(ViewHandler):
@@ -9,12 +8,4 @@ class Join(ViewHandler):
     """Join"""
 
     def get(self):
-        loader = template.Loader("templates")
-        self.render("_base.html",
-            global_meta_tags=ct_templates.global_meta_tags(),
-            title="Sign In",
-            bootstrap=ct_templates.load_bootstrap(),
-            navbar="",
-            banner_title=ct_templates.banner_title("Join a Room"),
-            body=loader.load("joinaroom.html").generate(rooms="")
-        )
+        self.render("joinaroom.html")
