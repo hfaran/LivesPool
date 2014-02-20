9fc2f4c71324c1bc80dbbc466f10fda74bd619dc
Steven Erenst <stevenerenst@gmail.com>
2014-02-20 00:13:59 -0800

    adding join room dialog

--- src/static/joinaroom.js
+++ src/static/joinaroom.js
@@ -1,8 +1,30 @@
-function join_room(name) {
+function join_room_window(name, pwd_req) {
+    $("#room_title").html(name);
+
+    if (pwd_req) {
+        $("#join").before('<input id="room_pwd" type="password" placeholder="password" />');
+    }
+
+    $("#join").click((function (name) {
+        return function() {
+            event.preventDefault();
+            data= {};
+            data['name'] = name;
+            if ($("#room_pwd").length != 0) {
+                data['password'] = $("#room_pwd").val();
+            }
+
+            join_room(JSON.stringify(data));
+        }
+    })(name));
+    $("#join_room_dialog").modal("show");
+}
+
+function join_room(data) {
     $.ajax({
         type: "POST",
         url: "/api/room/joinroom",
-        data: '{"name":"' + name + '"}',
+        data: data,
         success: function () {
             window.location.href ="/room/lobby";
         },
@@ -24,12 +46,12 @@ function append_rooms(data) {
     } else {
         $.each(data["data"], function(index, value) {
             var button = $($.parseHTML('<button class="btn btn-default btn-primary btn-block">' + value["name"] + '</button>'));
-            button.click((function (name) {
+            button.click((function (name, pwd_req) {
                 return function() {
                     event.preventDefault();
-                    join_room(name);
+                    join_room_window(name, pwd_req);
                 }
-            })(value["name"]));
+            })(value["name"], value["pwd_req"]));
 
             $("#room_container").append(button);
         });
