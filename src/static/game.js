366d68161404a895e4db44b6f017da92b0e26635
Ryan Wong <rmawong.92@gmail.com>
2014-04-22 03:03:26 -0700

    fixed bug in toggle; cleaned up game.js; added debouncer

--- src/static/game.js
+++ src/static/game.js
@@ -1,29 +1,29 @@
 'use strict';
 
+var NUM_BALLS = 15;
+
+var ID_TO_BALL = {
+    'ball-one': 1, 'ball-two': 2, 'ball-three': 3,
+    'ball-four': 4, 'ball-five': 5, 'ball-six': 6,
+    'ball-seven': 7, 'ball-eight': 8, 'ball-nine': 9,
+    'ball-ten': 10, 'ball-eleven': 11, 'ball-twelve': 12,
+    'ball-thirteen': 13, 'ball-fourteen': 14, 'ball-fifteen': 15
+};
+
+var BALL_IDS = [
+    'ball-one', 'ball-two', 'ball-three',
+    'ball-four', 'ball-five', 'ball-six',
+    'ball-seven', 'ball-eight', 'ball-nine',
+    'ball-ten', 'ball-eleven', 'ball-twelve',
+    'ball-thirteen', 'ball-fourteen','ball-fifteen'
+];
+
 $(document).ready(function() {
     get_player_balls();
+    on_click_ball();
     load_players(); // TODO: fix player list layout
     leave_game();
 });
-
-var indexToId = [
-    'placeholder',
-    'ball-one',
-    'ball-two',
-    'ball-three',
-    'ball-four',
-    'ball-five',
-    'ball-six',
-    'ball-seven',
-    'ball-eight',
-    'ball-nine',
-    'ball-ten',
-    'ball-eleven',
-    'ball-twelve',
-    'ball-thirteen',
-    'ball-fourteen',
-    'ball-fifteen'
-];
 
 function get_player_balls() {
     // Puts player_balls in sessionStorage so that highlight_player_balls
@@ -40,30 +40,15 @@ function get_player_balls() {
 }
 
 function load_balls() {
-    var ballsInPlay = {
-        'placeholder': false,
-        'ball-one': false,
-        'ball-two': false,
-        'ball-three': false,
-        'ball-four': false,
-        'ball-five': false,
-        'ball-six': false,
-        'ball-seven': false,
-        'ball-eight': false,
-        'ball-nine': false,
-        'ball-ten': false,
-        'ball-eleven': false,
-        'ball-twelve': false,
-        'ball-thirteen': false,
-        'ball-fourteen': false,
-        'ball-fifteen': false
-    };
-
     $.ajax({
         url: '/api/game/gamestate',
         success: function(data) {
-            $.each(data.data.balls_on_table, function(index, value) {
-                ballsInPlay[indexToId[value]] = true;
+            for(var j = 0; j < NUM_BALLS; j++) {
+                $('#' + BALL_IDS[j]).css('opacity', 0.3);
+            }
+
+            $.each(data.data.balls_on_table, function(key, value) {
+                $('#' + BALL_IDS[value-1]).css('opacity', 1);
             });
 
             if(data.data.winner != "") {
@@ -76,68 +61,36 @@ function load_balls() {
                     }
                 });
             }
-
-            $.each(ballsInPlay, function(index, value) {
-                if (value) {
-                    $('#' + index).css('opacity', 1);
-                } else {
-                    $('#' + index).css('opacity', 0.3);
-                }
-            });
         }
     }).done(function() {
         highlight_player_balls();
-        on_click_ball(ballsInPlay);
     });
-
     setTimeout(load_balls, 5000);
-    return ballsInPlay;
 }
 
-function on_click_ball(ballsInPlay) {
-    $('div.poolball').click(function() {
+function on_click_ball() {
+    $('div.poolball').click($.debounce(100, function(event) {
         var id = $(this).attr('id');
-        if (ballsInPlay[id]) {
-            $(this).fadeTo(700, 0.3);
-            ballsInPlay[id] = false;
-            toggle_ball(id);
-        } else {
-            $(this).fadeTo(700, 1);
-            ballsInPlay[id] = true;
-            toggle_ball(id);
-        }
-    });
+        toggle_ball(id, this);
+    }));
 }
 
-function toggle_ball(id) {
-    var idToBall = {
-        'ball-one': 1,
-        'ball-two': 2,
-        'ball-three': 3,
-        'ball-four': 4,
-        'ball-five': 5,
-        'ball-six': 6,
-        'ball-seven': 7,
-        'ball-eight': 8,
-        'ball-nine': 9,
-        'ball-ten': 10,
-        'ball-eleven': 11,
-        'ball-twelve': 12,
-        'ball-thirteen': 13,
-        'ball-fourteen': 14,
-        'ball-fifteen': 15
-    };
-
-    var num = idToBall[id];
+function toggle_ball(id, div) {
+    var num = ID_TO_BALL[id];
 
     $.ajax({
         type: 'POST',
         url: '/api/game/toggleball',
-        data: JSON.stringify({
-            ball: num
-        }),
+        data: JSON.stringify({ball: num}),
         success: function() {
-            console.log('Successful POST: ball #' + num);
+            console.log('Successful POST: ball #' + (num));
+        }
+    }).done(function() {
+        if($(div).css('opacity') == 1) {
+            $(div).fadeTo(700, 0.3);
+        }
+        else {
+            $(div).fadeTo(700, 1);
         }
     });
 }
@@ -182,7 +135,7 @@ function highlight_player_balls() {
     var player_balls = JSON.parse(sessionStorage.getItem('player_balls'));
 
     $.each(player_balls, function(k, v) {
-        var ball_id = '#' + String(indexToId[v]);
+        var ball_id = '#' + String(BALL_IDS[v-1]);
         $.each(["webkit", "moz"], function(n, i) {
             $(ball_id).css("-" + i + "-animation-iteration-count", "infinite");
         });
